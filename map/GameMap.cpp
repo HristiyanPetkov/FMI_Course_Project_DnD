dd9eccdcd80ad3e3f6121d026c049cf0a37228ef
HristiyanPetkov <hristiyanpetkov05@gmail.com>
2025-05-22 21:43:01 +0300

    [REFACTOR] GameMap works with the tile classes

--- map/GameMap.cpp
+++ map/GameMap.cpp
@@ -1,100 +1,151 @@
 #include <fstream>
+#include <random>
+
 #include "GameMap.hpp"
+#include "tile/WallTile.hpp"
+#include "tile/EmptyTile.hpp"
+#include "tile/CharacterTile.hpp"
+#include "tile/MonsterTile.hpp"
+#include "tile/TreasureTile.hpp"
 
-GameMap::GameMap(unsigned int level, const Character& player)
-: width(0), height(0), map(nullptr), items(), monsters(), player(player), characterX(0), characterY(0) {}
+GameMap::GameMap(unsigned int level)
+: width(10), height(10), numberOfMonsters(2), numberOfTreasures(2), map(), characterX(1), characterY(1) {
+    calculateLevelParameters(level);
+    map.resize(height, std::vector<Tile*>(width, nullptr));
 
-GameMap::GameMap(const Character& player, unsigned int level, const std::string &path)
-: width(0), height(0), map(nullptr), items(), monsters(), player(player), characterX(0), characterY(0) {
-    readMapFromFile(path);
-    setCharacterStartCoords();
-    setStartingMonsters(level);
-    setStartingItems();
+    fillMapWithEmptyAndWalls();
+    setStartingCharacterTile();
+    placeRandomElements(level);
+}
+
+void GameMap::calculateLevelParameters(unsigned int level) {
+    size_t width1 = 10, width2 = 15;
+    size_t height1 = 10, height2 = 10;
+    size_t numberOfMonsters1 = 2, numberOfMonsters2 = 3;
+    size_t numberOfTreasures1 = 2, numberOfTreasures2 = 2;
+
+    if(level == 0) {
+        throw std::invalid_argument("Starting level need to be at least 1");
+    }
+    if(level == 1) {
+        return;
+    }
+    for(unsigned i = 2; i < level; ++i) {
+        width2 += width1;
+        width1 = width2 - width1;
+
+        height2 += height1;
+        height1 = height2 - height1;
+
+        numberOfMonsters2 += numberOfMonsters1;
+        numberOfMonsters1 = numberOfMonsters2 - numberOfMonsters1;
+
+        numberOfTreasures2 += numberOfTreasures1;
+        numberOfTreasures1 = numberOfTreasures2 - numberOfTreasures1;
+    }
+
+    width = width2;
+    height = height2;
+    numberOfMonsters = numberOfMonsters2;
+    numberOfTreasures = numberOfTreasures2;
+}
+
+void GameMap::fillMapWithEmptyAndWalls() {
+    for (size_t i = 0; i < height; ++i) {
+        for (size_t j = 0; j < width; ++j) {
+            if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+                map[i][j] = new WallTile();
+            } else {
+                map[i][j] = new EmptyTile();
+            }
+        }
+    }
+}
+
+void GameMap::setStartingCharacterTile() {
+    delete map[characterY][characterX];
+    map[characterY][characterX] = new CharacterTile();
+}
+
+void GameMap::placeRandomElements(unsigned int level) {
+    std::vector<std::pair<size_t, size_t>> validPositions = getValidPositions();
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(validPositions.begin(), validPositions.end(), gen);
+
+    for(size_t i = 0; i < numberOfMonsters; ++i) {
+        auto [x, y] = validPositions.front();
+        delete map[y][x];
+        map[y][x] = new MonsterTile(Monster("Dragon", level));
+        validPositions.erase(validPositions.begin());
+    }
+    for(size_t i = 0; i < numberOfTreasures; ++i) {
+        auto [x, y] = validPositions.front();
+        delete map[y][x];
+        map[y][x] = TreasureTile::generateRandomTreasureTile(level);
+        validPositions.erase(validPositions.begin());
+    }
+}
+
+std::vector<std::pair<size_t, size_t>> GameMap::getValidPositions() {
+    std::vector<std::pair<size_t, size_t>> validPositions;
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            if (map[y][x]->isEmpty()) {
+                validPositions.emplace_back(x, y);
+            }
+        }
+    }
+
+    return validPositions;
+}
+
+GameMap::GameMap(const GameMap &other)
+: width(other.width), height(other.height), numberOfMonsters(other.numberOfMonsters),
+  numberOfTreasures(other.numberOfTreasures), map(),
+  characterX(other.characterX), characterY(other.characterY) {
+    map.resize(height, std::vector<Tile*>(width, nullptr));
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            if (other.map[y][x]) {
+                map[y][x] = other.map[y][x]->clone();
+            }
+        }
+    }
+}
+
+GameMap &GameMap::operator=(const GameMap &other) {
+    GameMap copy(other);
+    swap(copy);
+
+    return *this;
+}
+
+void GameMap::swap(GameMap &other) {
+    using std::swap;
+    swap(width, other.width);
+    swap(height, other.height);
+    swap(numberOfMonsters, other.numberOfMonsters);
+    swap(numberOfTreasures, other.numberOfTreasures);
+    swap(map, other.map);
+    swap(characterX, other.characterX);
+    swap(characterY, other.characterY);
+}
+
+GameMap::~GameMap() {
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            delete map[y][x];
+        }
+    }
 }
 
 void GameMap::render() {
-    for(size_t i = 0; i < width; ++i) {
-        for (size_t j = 0; j < height; ++j) {
-            std::cout << map[i][j];
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            std::cout << map[y][x]->getDisplayCharacter();
         }
         std::cout << std::endl;
     }
     std::cout << "Character coords: {" << characterX << ", " << characterY << "}" << std::endl;
-    player.print();
-    std::cout << "Monsters: " << std::endl;
-    for(const auto& monster : monsters) {
-        std::cout << monster.first.first << ", " << monster.first.second << ": ";
-        monster.second.print();
-    }
-    for(const auto& item : items) {
-        std::cout << item.first.first << ", " << item.first.second << ": ";
-        item.second->print();
-    }
-}
-
-void GameMap::movePlayer(Direction dir) {
-//    if(playerCanMove(dir)) {
-//        changePlayerCoords(dir);
-//    }
-}
-
-void GameMap::readMapFromFile(const std::string &path) {
-    std::ifstream file(path);
-    file >> width >> height;
-    std::cout << "Width: " << width;
-    map = new MapTile*[width];
-    for(size_t i = 0 ; i < width; ++i) {
-        map[i] = new MapTile[height];
-    }
-    for(size_t i = 0; i < width; ++i) {
-        for(size_t j = 0; j < height; ++j) {
-            file >> map[i][j];
-        }
-    }
-}
-
-void GameMap::setCharacterStartCoords() {
-    for(size_t i = 0; i < width; ++i) {
-        for(size_t j = 0; j < height; ++j) {
-            std::cout << map[i][j];
-            if(map[i][j].type == TileType::CHARACTER) {
-                characterX = i;
-                characterY = j;
-                return;
-            }
-        }
-    }
-    throw std::invalid_argument("Map doesn't contain a character");
-}
-
-void GameMap::setStartingMonsters(unsigned int level) {
-    for(size_t i = 0; i < width; ++i) {
-        for(size_t j = 0; j < height; ++j) {
-            if(map[i][j].type == TileType::MONSTER) {
-                std::pair<size_t, size_t> position = {i, j};
-                monsters.emplace(std::make_pair(i, j), Monster("Dragon", level));
-            }
-        }
-    }
-}
-
-void GameMap::setStartingItems() {
-    for(size_t i = 0; i < width; ++i) {
-        for(size_t j = 0; j < height; ++j) {
-            if(map[i][j].type == TileType::ITEM) {
-                std::pair<size_t, size_t> position = {i, j};
-                items.emplace(std::make_pair(i, j), new Item("Basic", 10));
-            }
-        }
-    }
-}
-
-GameMap::~GameMap() {
-    for(auto& pair : items) {
-        delete pair.second;
-    }
-    for(size_t i = 0; i < width; ++i) {
-        delete [] map[i];
-    }
-    delete [] map;
 }
--- map/GameMap.hpp
+++ map/GameMap.hpp
@@ -4,81 +4,36 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
+
 #include "../item/Item.hpp"
 #include "../monster/Monster.hpp"
 #include "../character/Character.hpp"
+#include "tile/Tile.hpp"
 
-enum class TileType { WALL, EMPTY, CHARACTER, ITEM, MONSTER };
 enum class Direction {UP, RIGHT, DOWN, LEFT};
-
-struct MapTile {
-    TileType type;
-    friend std::ostream& operator<<(std::ostream& os, const MapTile& mapTile);
-    friend std::istream& operator>>(std::istream& is, MapTile& mapTile);
-};
-
-inline std::ostream& operator<<(std::ostream& os, const MapTile& mapTile) {
-    switch(mapTile.type) {
-        case TileType::WALL:
-            return os << "#";
-        case TileType::EMPTY:
-            return os << ".";
-        case TileType::CHARACTER:
-            return os << "C";
-        case TileType::ITEM:
-            return os << "T";
-        case TileType::MONSTER:
-            return os << "M";
-    }
-    return os;
-}
-
-inline std::istream& operator>>(std::istream& is, MapTile& mapTile) {
-    char a;
-    is >> a;
-    switch(a) {
-        case '#':
-            mapTile.type = TileType::WALL;
-            break;
-        case '.':
-            mapTile.type = TileType::EMPTY;
-            break;
-        case 'C':
-            mapTile.type = TileType::CHARACTER;
-            break;
-        case 'T':
-            mapTile.type = TileType::ITEM;
-            break;
-        case 'M':
-            mapTile.type = TileType::MONSTER;
-            break;
-        default:
-            throw std::invalid_argument("Type type not supported");
-    }
-
-    return is;
-}
 
 class GameMap {
 public:
-    GameMap(unsigned level, const Character& player);
-    GameMap(const Character& player, unsigned int level, const std::string& path = "../map_assets/example.txt");
-    void render();
-    void movePlayer(Direction dir);
-
+    explicit GameMap(unsigned level);
+//    explicit GameMap(const std::string& path = "../map_assets/example.txt");
+    GameMap(const GameMap& other);
+    GameMap& operator=(const GameMap& other);
     ~GameMap();
+
+    void render();
+//    void movePlayer(Direction dir);
 private:
-    size_t width, height;
-    MapTile** map;
-    std::map<std::pair<size_t, size_t >, Item*> items;
-    std::map<std::pair<size_t, size_t >, Monster> monsters;
-    Character player;
+    size_t width, height, numberOfMonsters, numberOfTreasures;
+    std::vector<std::vector<Tile*>> map;
     size_t characterX, characterY;
 
-    void readMapFromFile(const std::string &basicString);
-    void setCharacterStartCoords();
-    void setStartingMonsters(unsigned int level);
-    void setStartingItems();
+    void swap(GameMap& other);
+    void calculateLevelParameters(unsigned level);
+    void fillMapWithEmptyAndWalls();
+    void setStartingCharacterTile();
+    void placeRandomElements(unsigned level);
+    std::vector<std::pair<size_t, size_t>> getValidPositions();
 };
 
 
