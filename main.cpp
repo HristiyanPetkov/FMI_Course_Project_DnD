642cb2fb6aee997adb2ca58c722d986e34d94bb4
HristiyanPetkov <hristiyanpetkov05@gmail.com>
2025-05-24 14:01:18 +0300

    [ADD] Option to run the game from file or level 1 at start

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
-#include "map/GameMap.hpp"
+#include "game/Game.hpp"
 
-int main() {
-    GameMap(Character("Pesho", CharacterClass::Human), 1).render();
+int main(int argc, char *argv[]) {
+    Game game;
+    if(argc > 1) {
+        game = Game::loadFromFile(argv[1]);
+    } else {
+        game = Game(1);
+    }
+    game.start();
 
     return 0;
 }
